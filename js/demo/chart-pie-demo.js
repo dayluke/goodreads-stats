0ee77f42ebe11576d8c45320421bf724a0daed12
Luke Day <ld227707@falmouth.ac.uk>
2020-08-20 13:43:28 +0100

    Corrected the tab spacing in the pie chart script

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -4,38 +4,38 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
 function loadPieChart(avgRating) {
-  var ctx = document.getElementById("myPieChart");
-  var myPieChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ['Average rating'],
-      datasets: [{
-        data: [avgRating, (5 - avgRating).toFixed(2)],
-        backgroundColor: ['#f6c23e', '#f8f9fc'],
-        hoverBackgroundColor: ['#f4b515', '#f1f3f9'],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-        filter: function (tooltipItem, data) {
-          if (data.labels[tooltipItem.index] === undefined) return false;
-          return true;
-      }
-      },
-      legend: {
-        display: false
-      },
-      cutoutPercentage: 80,
-    },
-  });
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Average rating'],
+            datasets: [{
+                data: [avgRating, (5 - avgRating).toFixed(2)],
+                backgroundColor: ['#f6c23e', '#f8f9fc'],
+                hoverBackgroundColor: ['#f4b515', '#f1f3f9'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+                filter: function (tooltipItem, data) {
+                    if (data.labels[tooltipItem.index] === undefined) return false;
+                    return true;
+                }
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+        },
+    });
 }
